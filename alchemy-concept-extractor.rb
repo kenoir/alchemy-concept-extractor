b54f4535a4fe859558d77d54ddb98e9916a7abf8
Robert Kenny <kenoir@gmail.com>
2012-09-24 20:40:43 +0100

    pushed extract and uris into ConceptExtractor class

--- alchemy-concept-extractor.rb
+++ alchemy-concept-extractor.rb
@@ -8,27 +8,50 @@ module AlchemyConceptExtractor
   require_relative 'lib/extractor.rb'
   require_relative 'lib/reporter.rb'
 
-  def self.uris(datfile_location)
-    uris_file = IO.read(datfile_location)
-    uris = uris_file.split("\n")
+  class ConceptExtractor
+    attr :api_key
+    attr :datfile_location
+    attr :outfiles_location
+    attr :rest_client
 
-    uris
+    def initialize(api_key,
+      datfile_location = "uris.dat",
+      outfiles_location = ".",
+      rest_client = RestClient)
+
+      @api_key = api_key
+      @datfile_location = datfile_location
+      @outfiles_location = outfiles_location
+      @rest_client = rest_client
+    end
+
+    def uris
+      uris_file = IO.read(@datfile_location)
+      uris = uris_file.split("\n")
+
+      uris
+    end
+
+    def extract
+      extractor = Extractor.new(@api_key,@rest_client)
+
+      uris.each do | uri |
+        concepts = extractor.get_concepts(uri)
+
+        refiner = Refiner.new
+        reporter = Reporter.new(concepts,refiner)
+
+        serialised_rdf = reporter.report(:ntriples) 
+
+        file_location = File.join(outfiles_location,File.basename(uri))
+        File.open(file_location, 'w') {|f| f.write(serialised_rdf) }
+      end
+    end
   end
 
   def self.extract(api_key,datfile_location,outfile_location,rest_client = RestClient)
-    extractor = Extractor.new(api_key,rest_client)
-
-    uris(datfile_location).each do | uri |
-      concepts = extractor.get_concepts(uri)
-
-      refiner = Refiner.new
-      reporter = Reporter.new(concepts,refiner)
-
-      serialised_rdf = reporter.report(:ntriples) 
-
-      file_location = File.join(outfiles_location,File.basename(uri))
-      File.open(file_location, 'w') {|f| f.write(serialised_rdf) }
-    end
+    concept_extractor = ConceptExtractor.new(api_key,datfile_location,outfile_location,rest_client)
+    concept_extractor.extract
   end
 
 end
--- spec/alchemy-concept-extractor_spec.rb
+++ spec/alchemy-concept-extractor_spec.rb
@@ -3,37 +3,54 @@ require 'spec_helper'
 
 describe AlchemyConceptExtractor do
 
-  describe AlchemyConceptExtractor, '#uris' do
-      
-    it 'return an array of URIs' do
-      uris = AlchemyConceptExtractor.uris(datfile_location)
-      uris.should be_an_instance_of(Array)
-      uris.count.should > 0
-      uris.first.should == "http://www.example.com/1"
+  describe AlchemyConceptExtractor::ConceptExtractor do
+
+    subject { AlchemyConceptExtractor::ConceptExtractor.new(
+      dummy_api_key,
+      datfile_location,
+      outfiles_location,
+      dummy_rest_client)
+    }
+  
+    describe AlchemyConceptExtractor::ConceptExtractor, '#uris' do
+      it 'return an array of URIs' do
+        uris = subject.uris
+        uris.should be_an_instance_of(Array)
+        uris.count.should > 0
+        uris.first.should == "http://www.example.com/1"
+      end
+    end
+
+    describe AlchemyConceptExtractor::ConceptExtractor, '#initialise' do
+      it 'should accept and set an API key, file location to read from and file location to write to' do
+        subject.api_key.should == dummy_api_key
+        subject.datfile_location.should == datfile_location
+        subject.outfiles_location.should == outfiles_location
+        subject.rest_client.should == dummy_rest_client
+      end
+    end
+
+    describe AlchemyConceptExtractor::ConceptExtractor, '#extract' do
+      it 'should create a set of valid rdf files' do
+        subject.extract
+
+        Dir["#{outfiles_location}/*"].each do | file_location |
+          graph = RDF::Graph.load(file_location)
+        end
+      end
     end
 
   end
 
   describe AlchemyConceptExtractor, '#extract' do
 
-    it 'should accept an API key, file location to read from andfile location to write to' do
-      AlchemyConceptExtractor.extract(
-        dummy_api_key,
-        datfile_location,
-        outfiles_location,
-        dummy_rest_client)
-    end
+    it 'should create a new instance of ConceptExtractor and call extract on it' do
+      concept_extractor = double('concept_extractor')
+      concept_extractor.should_receive(:extract)
 
-    it 'should create a set of valid rdf files' do
-      AlchemyConceptExtractor.extract(
-        dummy_api_key,
-        datfile_location,
-        outfiles_location,
-        dummy_rest_client)
-
-      Dir["#{outfiles_location}/*"].each do | file_location |
-        graph = RDF::Graph.load(file_location)
-      end
+      AlchemyConceptExtractor::ConceptExtractor.should_receive(:new) { concept_extractor }
+      
+      AlchemyConceptExtractor.extract(dummy_api_key,datfile_location,outfiles_location)
     end
 
   end
