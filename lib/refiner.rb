861e2ff72dffe71928289fb4f8b9730c307900cf
Phil Lamond <phil.lamond@gmail.com>
2012-09-24 23:02:56 +0100

    Work in progress refiner

--- lib/refiner.rb
+++ lib/refiner.rb
@@ -2,12 +2,31 @@ module AlchemyConceptExtractor
 
   class Refiner 
 
-    def initialize
-
-    end
-
     def refine(concepts)
-      RDF::Graph.new << [:hello, RDF::DC.title, "Hello, world!"]
+
+      rdf_graph = RDF::Graph.new
+
+      concepts['entities'].each do |entity|
+        if entity['type'] == 'Person'
+       
+          person_uri = ""
+          if entity['disambiguated'].nil?
+            person_uri = entity['dbpedia']
+          else
+            person_uri = entity['disambiguated']['dbpedia']
+          end
+      
+          name = entity['text']
+          s = RDF::URI.new(person_uri)
+          p = FOAF.name
+          o = RDF::Literal.new(name)
+          person_statement = RDF::Statement.new(s, p, o)
+
+          rdf_graph.insert person_statement
+        end
+      end
+
+      rdf_graph
     end
 
   end
