5e440bd751413f817df5e42b49ef8d92b3b0aa37
Robert Kenny <kenoir@gmail.com>
2012-09-25 18:59:59 +0100

    pretty DRY

--- lib/refiner.rb
+++ lib/refiner.rb
@@ -3,106 +3,105 @@ module AlchemyConceptExtractor
   class Refiner 
 
     def refine(concepts)
-
       rdf_graph = RDF::Graph.new
+      all_triples = Array.new
 
-      concepts['entities'].each do |entity|
-
-        if entity['type'] == 'Person'
-          person_uri = nil
-          if entity.has_key? 'disambiguated'
-            person_uri = entity['disambiguated']['dbpedia']
-          end
-
-          # TODO: Should we make a further attempt at disambiguating?
-          next if person_uri.nil?
-
-          name = entity['text']
-          subject = RDF::URI.new(person_uri)
-
-          rdf_graph.insert person_name_statement(subject,name) 
-          rdf_graph.insert person_type_statement(subject)
-        elsif entity['type'] == 'Country' || entity['type'] == 'City'
-          place_uri = nil
-          if entity.has_key? 'disambiguated'
-            place_uri = entity['disambiguated']['dbpedia']
-          end
-
-          next if place_uri.nil?
-
-          name = entity['text']
-          subject = RDF::URI.new(place_uri)
-
-          rdf_graph.insert place_name_statement(subject,name) 
-          rdf_graph.insert place_type_statement(subject)
+      concepts['entities'].each do | entity |
+        case entity['type']
+        when "City" || "Country" 
+          all_triples.concat triples(entity) { place_triples(entity) }
+        when "Person" 
+          all_triples.concat triples(entity) { person_triples(entity) }
         end
+      end
+
+      statements(all_triples).each do | statement |
+        rdf_graph.insert statement  
       end
 
       rdf_graph
     end
 
     private 
-    def insert_statement(statement)
-      rdf_graph.insert statement
+    def triples(entity)
+      triples = Array.new
+      uri = disambiguated_uri(entity)
+      return triples if uri.nil?
+
+      new_triples = yield
+      new_triples.each do | key,triple |
+        triples.push(triple)
+      end
+
+      triples
     end
 
     private
-    def place_name_statement(subject,name)
-      object_name = RDF::Literal.new(name)
-      predicate_name = RDFS.label
+    def statements(triples)
+      statements = Array.new
 
-      place_statement = RDF::Statement.new(
-        subject, 
-        predicate_name, 
-        object_name
-      )
+      triples.each do | triple |
+        statement = RDF::Statement.new(
+          triple[:subject], 
+          triple[:predicate],
+          triple[:object]
+        )
+        statements.push(statement)
+      end
 
-      place_statement
+      statements
     end
-
+    
     private
-    def place_type_statement(subject)
-      predicate_type = RDF.type
+    def place_triples(place)
+      name = place['text']
       event = RDF::Vocabulary.new("http://purl.org/NET/c4dm/event.owl")
-      object_place = event.place
 
-      type_statement = RDF::Statement.new(
-        subject,
-        predicate_type,
-        object_place
-      )
+      triples = {
+        :name => {
+          :subject => RDF::URI.new(place['disambiguated']['dbpedia']),
+          :object => RDF::Literal.new(name),
+          :predicate => RDFS.label
+        },
+        :type => {
+          :subject => RDF::URI.new(place['disambiguated']['dbpedia']),
+          :object => event.place,
+          :predicate => RDF.type
+        }
+      }
 
-      type_statement
+      triples
     end
 
     private 
-    def person_name_statement(subject,name)
-      object_name = RDF::Literal.new(name)
-      predicate_name = FOAF.name
+    def person_triples(person)
+      name = person['text']
 
-      person_statement = RDF::Statement.new(
-        subject, 
-        predicate_name, 
-        object_name
-      )
+      triples = {
+        :name => {
+          :subject => RDF::URI.new(person['disambiguated']['dbpedia']),
+          :object => RDF::Literal.new(name),
+          :predicate => FOAF.name
+        },
+        :type => {
+          :subject => RDF::URI.new(person['disambiguated']['dbpedia']),
+          :object => FOAF.Person,
+          :predicate => RDF.type 
+        }
 
-      person_statement
+      }
+
+      triples
     end
 
-    private
-    def person_type_statement(subject)
-      predicate_type = RDF.type
-      object_person = FOAF.Person
-
-      type_statement = RDF::Statement.new(
-        subject,
-        predicate_type,
-        object_person
-      )
-
-      type_statement
+    private 
+    def disambiguated_uri(entity)
+      uri = nil
+      if entity.instance_of?(Hash) and entity.has_key? 'disambiguated'
+        uri = entity['disambiguated']['dbpedia']
+      end
+    
+      uri
     end
-
   end
-
 end
